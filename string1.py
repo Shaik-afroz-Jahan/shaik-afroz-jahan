d95e575022b9b011e7a4d68e960ac18946dd1e9a
Shaik-afroz-Jahan <120170159+Shaik-afroz-Jahan@users.noreply.github.com>
2022-12-14 14:14:27 +0530

    Add files via upload

---
+++ string1.py
@@ -0,0 +1,2 @@
+str="Twinkle,twinkle,little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tlike a diamond in the sky.\ntwinkle,twinkle,little star,\n\tHow I wonder what you are!"
+print(str)
